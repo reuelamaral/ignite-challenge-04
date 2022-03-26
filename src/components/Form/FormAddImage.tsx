6085f62c1c0f5bbc0799be74745900ae623078d3
Reuel Amaral <reuelsamaral@gmail.com>
2022-03-26 14:33:41 -0300

    Challenge completed

---
+++ src/components/Form/FormAddImage.tsx
@@ -0,0 +1,131 @@
+import { Box, Button, Stack, useToast } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { useMutation, useQueryClient } from 'react-query';
+
+import { api } from '../../services/api';
+import { FileInput } from '../Input/FileInput';
+import { TextInput } from '../Input/TextInput';
+
+interface FormImageProps {
+  image: FileList;
+  title: string;
+  description: string;
+  url: string;
+}
+
+interface FormAddImageProps {
+  closeModal: () => void;
+}
+
+export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
+  const [imageUrl, setImageUrl] = useState('');
+  const [localImageUrl, setLocalImageUrl] = useState('');
+  const toast = useToast();
+  const regex = new RegExp(/\.(jpeg|jpg|png|gif)$/i);
+
+  const formValidations = {
+    image: {
+      required: 'Arquivo obrigatório',
+      validate: {
+        lessThan10MB: file =>
+          file[0].size < 10000000 || 'O arquivo deve ser menor que 10MB',
+        acceptedFormats: file =>
+          regex.test(file[0].name) ||
+          'Somente são aceitos arquivos PNG, JPEG e GIF',
+      },
+    },
+    title: {
+      required: 'Título obrigatório',
+      minLength: { value: 2, message: 'Mínimo de 2 caracteres' },
+      maxLength: { value: 20, message: 'Máximo de 20 caracteres' },
+    },
+    description: {
+      required: 'Descrição obrigatória',
+      maxLength: { value: 65, message: 'Máximo de 65 caracteres' },
+    },
+  };
+
+  const queryClient = useQueryClient();
+  const mutation = useMutation(
+    (payload: FormImageProps) => api.post('/api/images', payload),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries('images');
+      },
+    }
+  );
+
+  const { register, handleSubmit, reset, formState, setError, trigger } =
+    useForm();
+  const { errors } = formState;
+
+  const onSubmit = async (data: FormImageProps): Promise<void> => {
+    try {
+      if (!imageUrl) {
+        toast({
+          status: 'error',
+          title: 'Imagem não enviada',
+          isClosable: true,
+        });
+        return;
+      }
+
+      mutation.mutate(data);
+
+      toast({
+        title: 'Imagem enviada com sucesso',
+        status: 'success',
+      });
+    } catch {
+      toast({
+        title: 'Erro ao enviar a imagem',
+        status: 'error',
+        isClosable: true,
+      });
+    } finally {
+      reset();
+      closeModal();
+      setImageUrl('');
+    }
+  };
+
+  return (
+    <Box as="form" width="100%" onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={4}>
+        <FileInput
+          setImageUrl={setImageUrl}
+          localImageUrl={localImageUrl}
+          setLocalImageUrl={setLocalImageUrl}
+          setError={setError}
+          trigger={trigger}
+          {...register('image', formValidations.image)}
+          error={errors.image}
+        />
+
+        <TextInput
+          placeholder="Título da imagem..."
+          {...register('title', formValidations.title)}
+          error={errors.title}
+        />
+
+        <TextInput
+          placeholder="Descrição da imagem..."
+          {...register('description', formValidations.description)}
+          error={errors.description}
+        />
+      </Stack>
+
+      <Button
+        my={6}
+        isLoading={formState.isSubmitting}
+        isDisabled={formState.isSubmitting}
+        type="submit"
+        w="100%"
+        py={6}
+      >
+        Enviar
+      </Button>
+    </Box>
+  );
+}
---
+++ src/components/Modal/ViewImage.tsx
@@ -0,0 +1,36 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalFooter,
+  Image,
+  Link,
+} from '@chakra-ui/react';
+
+interface ModalViewImageProps {
+  isOpen: boolean;
+  onClose: () => void;
+  imgUrl: string;
+}
+
+export function ModalViewImage({
+  isOpen,
+  onClose,
+  imgUrl,
+}: ModalViewImageProps): JSX.Element {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+
+      <ModalContent bgColor="pGray.600">
+        <Image src={imgUrl} maxH={600} maxW={900} />
+
+        <ModalFooter justifyContent="flex-start">
+          <Link href={imgUrl} target="_blank">
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
